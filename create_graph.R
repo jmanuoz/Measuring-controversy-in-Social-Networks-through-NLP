5c59602ba8ad09abb7683e76aa997e18edde814e
juanma <jmanuoz90@gmail.com>
2020-05-11 15:53:10 -0300

    correccion generacion grafo

--- create_graph.R
+++ create_graph.R
@@ -13,11 +13,7 @@ create_token(
 tweets.df <- search_tweets("search", n=250000,token=twitter_token,retryonratelimit = TRUE)
 
 #CREO EL GRAFO DE RETWEETS
-filter(tweets.df, retweet_count > 0) %>% 
-  select(screen_name, mentions_screen_name) %>%
-  unnest(mentions_screen_name) %>% 
-  filter(!is.na(mentions_screen_name)) %>% 
-  graph_from_data_frame() -> net
+net = network_graph(tweets.df, .e = c("retweet"))
 
 save.image("datasets/filename.RData")
 #GENERO EL LAYOUT (PARA VISUALIZARLO)
